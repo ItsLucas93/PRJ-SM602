032d80759d88e0c09294ae5842a0997254288b09
Lucas <45604254+ItsLucas93@users.noreply.github.com>
2024-04-28 23:11:10 +0200

    Update balashammer.py

--- algorithms/balashammer.py
+++ algorithms/balashammer.py
@@ -5,6 +5,89 @@ Description: Ce fichier contient l'algorithme de Balas-Hammer
 Version de Python: 3.12
 """
 
+from tabulate import tabulate
+from display_tab import display_tab_matrix
 
 def balashammer(tab_matrix):
-    pass
+    # Initialisation des variables
+    num_provisions = len(tab_matrix[1])
+    num_orders = len(tab_matrix[2])
+
+    # Deep copy of the tab_matrix
+    balas_hammer_matrix = [[[0, tab_matrix[0][i][j][1]] for j in range(0, num_orders)] for i in range(0, num_provisions)]
+    list_provisions = [tab_matrix[1][i] for i in range(0, num_provisions)]
+    list_orders = [tab_matrix[2][j] for j in range(0, num_orders)]
+
+    while sum(list_provisions) != 0 and sum(list_orders) != 0:
+        penalties = []
+
+        for i in range(num_provisions):
+            row = [tab_matrix[0][i][j][1] for j in range(num_orders) if list_provisions[i] > 0 if balas_hammer_matrix[i][j][1] > -1]
+            if len(row) > 1:
+                row = sorted(row)
+                penalties.append((row[1] - row[0], i, "row"))
+            else:
+                penalties.append((float('-inf'), i, None))
+
+        for j in range(num_orders):
+            column = [tab_matrix[0][i][j][1] for i in range(num_provisions) if list_orders[j] > 0 if balas_hammer_matrix[i][j][1] > -1]
+            if len(column) > 1:
+                column = sorted(column)
+                penalties.append((column[1] - column[0], j, "column"))
+            else:
+                penalties.append((float('-inf'), j, None))
+
+        # Max Penalty
+        max_penalty, max_index, mode = max_penalties(penalties)
+        min_quantity, min_index = float('inf'), -1
+
+        match mode:
+            case "row":
+                for j in range(num_orders):
+                    if list_orders[j] > 0 and tab_matrix[0][max_index][j][1] < min_quantity:
+                        min_quantity = tab_matrix[0][max_index][j][1]
+                        min_index = j
+                quantity = min(list_provisions[max_index], list_orders[min_index])
+                balas_hammer_matrix[max_index][min_index][0] += quantity
+                list_provisions[max_index] -= quantity
+                list_orders[min_index] -= quantity
+                if list_provisions[max_index] == 0:
+                    for j in range(num_orders):
+                        balas_hammer_matrix[max_index][j][1] = -1
+            case "column":
+                for i in range(num_provisions):
+                    if list_provisions[i] > 0 and tab_matrix[0][i][max_index][1] < min_quantity:
+                        min_quantity = tab_matrix[0][i][max_index][1]
+                        min_index = i
+                quantity = min(list_provisions[min_index], list_orders[max_index])
+                balas_hammer_matrix[min_index][max_index][0] += quantity
+                list_provisions[min_index] -= quantity
+                list_orders[max_index] -= quantity
+                if list_orders[max_index] == 0:
+                    for i in range(num_provisions):
+                        balas_hammer_matrix[i][max_index][1] = -1
+            case _:
+                pass
+
+        # Affichage à chaque étape
+        # display_tab_matrix([balas_hammer_matrix, tab_matrix[1], tab_matrix[2]], "Balas-Hammer")
+
+    # Réinitialisation des coûts à l'original
+    for i in range(num_provisions):
+        for j in range(num_orders):
+            if balas_hammer_matrix[i][j][1] == -1:
+                balas_hammer_matrix[i][j][1] = tab_matrix[0][i][j][1]
+
+    return [balas_hammer_matrix, tab_matrix[1], tab_matrix[2]]
+
+
+def max_penalties(penalties):
+    max_penalty = float('-inf')
+    max_index = -1
+    mode = None
+    for i in range(len(penalties)):
+        if penalties[i][0] > max_penalty:
+            max_penalty = penalties[i][0]
+            max_index = penalties[i][1]
+            mode = penalties[i][2]
+    return max_penalty, max_index, mode
