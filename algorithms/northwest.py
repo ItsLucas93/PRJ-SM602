317070b3a2a3d771aab8203894380b5a1a934230
Lucas <45604254+ItsLucas93@users.noreply.github.com>
2024-05-05 10:01:41 +0200

    Update northwest.py

--- algorithms/northwest.py
+++ algorithms/northwest.py
@@ -7,6 +7,13 @@ Version de Python: 3.12
 
 
 def northwest(tab_matrix):
+    """
+    * Fonction : northwest
+    * -------------------
+    * Cette fonction implémente l'algorithme de Nord-Ouest pour résoudre le problème de transport
+    * :param tab_matrix: La matrice du problème de transport
+    * :return: La matrice du problème de transport résolu
+    """
     # Initialisation des variables
     num_provisions = len(tab_matrix[1])
     num_orders = len(tab_matrix[2])
@@ -16,11 +23,10 @@ def northwest(tab_matrix):
     list_provisions = [tab_matrix[1][i] for i in range(0, num_provisions)]
     list_orders = [tab_matrix[2][j] for j in range(0, num_orders)]
 
-    # print(northwest_matrix)
-
+    # Algorithme de Nord-Ouest
     i, j = 0, 0
     while i < num_provisions and j < num_orders:
-        # print([northwest_matrix, list_provisions, list_orders], i, j, min(list_provisions[i],  list_orders[j]))
+        # Si une des deux listes est vide, on passe à la suivante
         if list_provisions[i] == 0:
             i += 1
             continue
@@ -28,11 +34,13 @@ def northwest(tab_matrix):
             j += 1
             continue
 
+        # Calcul de la quantité à transporter
         quantity = min(list_provisions[i], list_orders[j])
         northwest_matrix[i][j][0] = quantity
         list_provisions[i] -= quantity
         list_orders[j] -= quantity
 
+        # Passage à la case suivante
         if list_provisions[i] == 0 and list_orders[j] == 0:
             i += 1
             j += 1
